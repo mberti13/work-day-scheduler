135cbf695ff9360f91b480f1eba0fab9d5d99a5d
MJB <matt.berti13@gmail.com>
2022-03-15 11:51:40 -0500

    updated README

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-<<<<<<< HEAD
-# Work Day Scheduler Starter Code
-=======
-# work-day-scheduler
->>>>>>> 510eabe184ce8135a86e93f5daee9a29b4de1ac7
+# Work Day Scheduler
+
+## Description
+
+This project is an online web application designed to resemble one's daily business planner. With this application, the user can store tasks based on the hourly timeblocks throughout the business day(9am-5pm). This project uses Bootstrap and JQuery in order to complete the layout, as well as save tasks to each block and change the time-blocks colors based on the current time.
+
+### Screenshot:
+![image](./screenshot.jpg "Screenshot of deployed app.")
+
+
+
+
+**Link to Deployed Application:**
+https://mberti13.github.io/password-generator/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,39 @@
 var currentDay = document.querySelector("#currentDay");
 var containerEl = document.querySelector(".container");
 
+
 //Sets current date at top of page
 currentDay.innerHTML = moment().format("dddd, MMMM Do");
 
-// 
+
+
+//Create a task
+$(".saveBtn").on("click", function(){
+    var parent = $(this).parent();
+    var id = parent.attr("id");
+    var text = parent.find("textarea").val();
+    localStorage.setItem(id, text);
+})
+
+
+//retrieves tasks
+for(var i = 9; i < 18; i++){
+    var text = localStorage.getItem(i);
+    //sets i = to #id
+    $("#"+i).find("textarea").val(text);
+}
+//sets current hour of the day
+var currentHour = moment().hour();
+//Sets color based on current time to calendar/schedule 
+$(".time-block").each(function(){
+    var timeBlockHour = $(this).attr("id");
+    var textBlock = $(this).find(".description");
+    if(currentHour == timeBlockHour){
+        textBlock.addClass("present");
+    }else if(currentHour > timeBlockHour){
+        textBlock.addClass("past");
+    }else{
+        textBlock.addClass("future");
+    }
+    
+})
